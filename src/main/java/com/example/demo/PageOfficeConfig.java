b2f4981c39caad8530db05dbdb64724222bd1280
panfengping <Fengping.Pan@geely.com>
2021-09-18 15:35:40 +0800

    格式

--- src/main/java/com/example/demo/PageOfficeConfig.java
+++ src/main/java/com/example/demo/PageOfficeConfig.java
@@ -1,9 +1,5 @@
 package com.example.demo;
 
-import cn.hutool.core.date.DateUtil;
-
-import java.util.Date;
-
 /**
  * @author Fengping.Pan
  */
@@ -11,7 +7,7 @@ import java.util.Date;
 public class PageOfficeConfig {
 
     public static void main(String[] args) {
-        System.out.println(DateUtil.endOfDay(new Date()).toDateStr());
+    
     }
 
 }
